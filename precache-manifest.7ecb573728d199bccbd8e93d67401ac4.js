56ef0ee33eb14f42d0ff607602c51ab7b21f0d96
SolanaSummer <cryptocrush@protonmail.com>
2021-05-16 22:46:32 +0200

    Updates

---
+++ precache-manifest.7ecb573728d199bccbd8e93d67401ac4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "820eb110148c06c3e19360048ce8d9c0",
+    "url": "/serum-dex-ui/index.html"
+  },
+  {
+    "revision": "617722b3eefadfbbae3c",
+    "url": "/serum-dex-ui/static/css/2.3a0ac4c9.chunk.css"
+  },
+  {
+    "revision": "c4bf3b6fccdd6a013631",
+    "url": "/serum-dex-ui/static/css/main.0d007200.chunk.css"
+  },
+  {
+    "revision": "617722b3eefadfbbae3c",
+    "url": "/serum-dex-ui/static/js/2.4e2505fe.chunk.js"
+  },
+  {
+    "revision": "d2e09aded9686c343e34a065031febca",
+    "url": "/serum-dex-ui/static/js/2.4e2505fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4bf3b6fccdd6a013631",
+    "url": "/serum-dex-ui/static/js/main.93383f98.chunk.js"
+  },
+  {
+    "revision": "372bb5c3396519f2c750",
+    "url": "/serum-dex-ui/static/js/runtime-main.d91a094d.js"
+  },
+  {
+    "revision": "49174c736a9c871baaeb2db6d5372eee",
+    "url": "/serum-dex-ui/static/media/logo.49174c73.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1f4d70c287d1552579cbaadafc778654.js"
+  "/serum-dex-ui/precache-manifest.7ecb573728d199bccbd8e93d67401ac4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/serum-dex-ui/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
